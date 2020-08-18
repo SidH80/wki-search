36fae833afd08c058c725bae3500838630fff94c
lethalSid <senien.hicks@gmail.com>
2020-08-18 18:29:52 -0400

    committing service call and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { WikipediaService } from './wikipedia.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  onTerm(event: string) {
-    console.log(`i am the app and here is the term ${event}`);
+  pages = [];
+  constructor(
+    private wikipedia: WikipediaService,
+  ) { }
+
+  onTerm(term: string):void {
+    this.wikipedia.search(term).subscribe((response: any) => {
+      this.pages = response.query.search
+    })
   }
 }
