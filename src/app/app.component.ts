1efd2314c54734641e43a26c71de32e0380558ab
lethalSid <senien.hicks@gmail.com>
2020-08-19 00:12:17 -0400

    added into search method a wikipedia interface as a generic type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,15 @@ import { WikipediaService } from './wikipedia.service';
 })
 export class AppComponent {
   pages = [];
+
+
   constructor(
     private wikipedia: WikipediaService,
   ) { }
 
   onTerm(term: string):void {
-    this.wikipedia.search(term).subscribe((response: any) => {
-      this.pages = response.query.search
-    })
+    this.wikipedia.search(term).subscribe((pages) => {
+      this.pages = pages;
+    });
   }
 }
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { pluck } from 'rxjs/operators';
+
+interface WikipediaResponse {
+  query: {
+    search: {
+      title: string;
+      snippet: string;
+      pageid: number;
+    }[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +22,18 @@ export class WikipediaService {
   ) { }
 
   search(term: string) {
-    return this.httpClient.get('https://en.wikipedia.org/w/api.php?', {
-      params: {
-        action: 'query',
-        format: 'json',
-        list: 'search',
-        utf8: '1',
-        srsearch: term,
-        origin: '*'
-      }
-    });
+    return this.httpClient
+      .get<WikipediaResponse>('https://en.wikipedia.org/w/api.php?', {
+        params: {
+          action: 'query',
+          format: 'json',
+          list: 'search',
+          utf8: '1',
+          srsearch: term,
+          origin: '*'
+        }
+      }).pipe(
+          pluck('query', 'search')
+      )
   }
 }
